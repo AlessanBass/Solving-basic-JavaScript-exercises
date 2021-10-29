e1b25f1d4e9c9f55d51ab3e288d736001ef86dc4
Alessandro Conceição Santos <alessanbaixista@gmail.com>
2021-10-29 12:04:42 -0300

    Completar o ciclo

---
+++ 10 - Exercicio.js
@@ -0,0 +1,10 @@
+/**
+ * Crie uma função que verifica se um número inteiro passado como parêmetro é divisível por 3 e retorne true
+ou false.
+ */
+ verifica = (a)=>{
+     if (a % 3 == 0){
+         return true
+     }else return false
+ }
+console.log(verifica(12))
